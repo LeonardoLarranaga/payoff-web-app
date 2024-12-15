69f93da2806bccb8aa6025564395991b10ea3c98
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-12-14 17:18:39 -0800

    Sign in with OTP

--- app/page.js
+++ app/page.js
@@ -1,65 +1,15 @@
 'use client'
 
-import {Button, Image, Input, Tab, Tabs} from "@nextui-org/react"
-import {useEffect, useState} from "react"
-import pocketbase from "@/libraries/pocketbase"
+import {Button, Image, Input, InputOtp} from "@nextui-org/react"
+import {useState} from "react"
+import {useAuth} from "@/contexts/auth-context";
 
 export default function Home() {
 
-    const [selectedTab, setSelectedTab] = useState("signin")
-
     const [email, setEmail] = useState("")
-    const [name, setName] = useState("")
     const [otp, setOtp] = useState("")
-
-    const validateEmail = (email) => {
-        return String(email)
-            .toLowerCase()
-            .match(
-                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            )
-    }
-
-    const handleSignIn = async () => {
-        if (selectedTab === "signup" && !name.trim()) {
-            alert("Please enter your name")
-            return
-        }
-
-        if (!validateEmail(email)) {
-            alert("Please enter a valid email address")
-            return
-        }
-
-        try {
-
-        } catch (error) {
-
-        }
-    }
-
-    const loginPortion = (
-    <div className="flex flex-col gap-4 items-center justify-center pt-4 sm:grid sm:grid-cols-6 sm:gap-2 w-full sm:w-auto max-w-sm">
-        <Input
-            label="Email"
-            type="email"
-            placeholder="Sign in with your email"
-            variant="flat"
-            className="sm:col-span-4 md:w-full px-8 sm:px-0"
-            isInvalid={false}
-            isClearable
-            onValueChange={setEmail}
-        />
-
-        <Button
-            className="sm:col-span-2 hover:shadow-2xl sm:h-full"
-            color="primary"
-            onPress={handleSignIn}
-        >
-            Receive OTP Code
-        </Button>
-    </div>
-    )
+    const [isInvalid, setIsInvalid] = useState(false)
+    const {otpResponse, requestSignInWithOTP, signInWithOTP} = useAuth()
 
     return (
         <div className="flex flex-col justify-center items-center h-screen text-center w-full gradient-container">
@@ -76,28 +26,41 @@ export default function Home() {
                 Payoff (but it's a web app)
             </h1>
 
-            <Tabs
-                aria-label="Sign in or sign up"
-                className="pt-4"
-                onSelectionChange={setSelectedTab}
-            >
-                <Tab key="signin" title="Sign In" className="w-full">
-                    {loginPortion}
-                </Tab>
-                <Tab key="signup" title="Sign Up" className="w-full">
-                    <Input
-                        label="Name"
-                        type="name"
-                        placeholder="What should we call you?"
-                        variant="flat"
-                        className="px-8 sm:px-0 pt-4 w-full"
-                        isInvalid={false}
-                        isClearable
-                        onValueChange={setName}
+            <div
+                className="flex flex-col gap-4 items-center justify-center pt-4 sm:grid sm:grid-cols-6 sm:gap-2 w-full sm:w-auto max-w-sm">
+                <Input
+                    label="Email"
+                    type="email"
+                    placeholder="Sign in with your email"
+                    variant="flat"
+                    className="sm:col-span-4 md:w-full px-8 sm:px-0"
+                    isInvalid={false}
+                    isClearable
+                    onValueChange={setEmail}
+                />
+
+                <Button
+                    className="sm:col-span-2 hover:shadow-2xl sm:h-full"
+                    color="primary"
+                    onPress={() => requestSignInWithOTP(email)}
+                >
+                    Receive OTP Code
+                </Button>
+            </div>
+
+            {otpResponse && (
+                <div>
+                    <InputOtp
+                        length={6}
+                        onValueChange={setOtp}
+                        className="pt-4"
+                        isInvalid={isInvalid}
+                        onComplete={() => signInWithOTP(email, otp, setOtp, setIsInvalid)}
                     />
-                    {loginPortion}
-                </Tab>
-            </Tabs>
+
+                    {isInvalid && <p className="text-red-500">Invalid OTP Code</p>}
+                </div>
+            )}
         </div>
     )
 }
--- contexts/auth-context.jsx
+++ contexts/auth-context.jsx
@@ -3,6 +3,7 @@
 import {createContext, useContext, useEffect, useState} from "react"
 import {useRouter} from "next/navigation"
 import pocketbase from "@/libraries/pocketbase";
+import {ClientResponseError} from "pocketbase";
 
 const AuthContext = createContext(null)
 
@@ -11,19 +12,18 @@ export const useAuth = () => {
 }
 
 export const AuthProvider = ({children}) => {
-    const [authData, setAuthData] = useState(null)
-    const [name, setName] = useState(null)
     const [email, setEmail] = useState(null)
+    const [token, setToken] = useState(null)
+    const [otpResponse, setOtpResponse] = useState("")
 
     const router = useRouter()
 
     useEffect(() => {
         try {
+            if (!token || !email)
             if (pocketbase.authStore.token && pocketbase.authStore.isValid) {
-                setAuthData(pocketbase.authStore.model)
-                setName(localStorage.getItem('auth.name'))
                 setEmail(localStorage.getItem('auth.email'))
-
+                setToken(localStorage.getItem('auth.token'))
                 if (window.location.pathname === '/') router.push("home")
             } else clearCredentials()
         } catch (error) {
@@ -34,16 +34,92 @@ export const AuthProvider = ({children}) => {
 
     function clearCredentials() {
         pocketbase.authStore.clear()
-        localStorage.removeItem('auth.name')
         localStorage.removeItem('auth.email')
-        setAuthData(null)
-        setName(null)
+        localStorage.removeItem('auth.token')
         setEmail(null)
+        setToken(null)
+        setOtpResponse(null)
         router.push('/')
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+    }
+
+    const requestSignInWithOTP = async (email) => {
+        if (!validateEmail(email)) {
+            alert("Please enter a valid email address")
+            return
+        }
+
+        let shouldSendOtp = false
+
+        try {
+            const password = self.crypto.randomUUID()
+            await pocketbase.collection("users").create({
+                email: email,
+                password: password,
+                passwordConfirm: password
+            })
+            shouldSendOtp = true
+        } catch (error) {
+            if (error instanceof ClientResponseError &&
+                error.status === 400 &&
+                error.response?.data?.email?.code === "validation_not_unique") {
+                shouldSendOtp = true
+            } else {
+                alert("An error occurred while trying to sign in")
+                console.error(error)
+            }
+        }
+
+        if (!shouldSendOtp) return
+
+        const otpResponse = await pocketbase.collection("users").requestOTP(email)
+        if (!otpResponse.otpId) {
+            alert("An error occurred while trying to request the OTP")
+            return
+        }
+
+        setOtpResponse(otpResponse.otpId)
+    }
+
+    const signInWithOTP = async (email, otp, setOtp, setIsInvalid) => {
+        try {
+            const auth = await pocketbase.collection("users").authWithOTP(otpResponse, otp)
+            setOtp("")
+            if (!auth.token) {
+                alert("An error occurred while trying to sign in")
+                setOtp("")
+                setIsInvalid(true)
+                return
+            }
+            localStorage.setItem('auth.email', email)
+            localStorage.setItem('auth.token', auth.token)
+            pocketbase.authStore.save(auth.token, auth.record)
+            setOtpResponse(null)
+            router.push("home")
+        } catch (error) {
+            console.error(error)
+            setIsInvalid(true)
+            setOtp("")
+        }
+    }
+
+    const value = {
+        email,
+        token,
+        otpResponse,
+        requestSignInWithOTP,
+        signInWithOTP,
+    }
+
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={value}>
             {children}
         </AuthContext.Provider>
     )
