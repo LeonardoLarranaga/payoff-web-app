ac28e0f28bd8d953f23e602ec3e5fc947e5d6987
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-12-16 21:53:44 -0800

    - Fixes padding issues

--- app/main-content.jsx
+++ app/main-content.jsx
@@ -1,12 +1,14 @@
 import {motion} from "framer-motion"
 import {useNavigation} from "@/contexts/navigation-context"
+import {usePathname} from "next/navigation"
 
 export default function MainContent({children}) {
     const { isNavigationMenuOpen } = useNavigation();
+    const pathname = usePathname()
 
     return (
         <motion.div
-            className="pt-1 pl-1"
+            className={pathname !== "/" && window.innerWidth >= 700 ? "pt-1 pl-1" : ""}
             animate={{
                 marginLeft: isNavigationMenuOpen && window.innerWidth >= 700 ? '14rem' : '0',
             }}
