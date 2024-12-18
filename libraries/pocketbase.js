93f8c4ae856bc2f338e04175f49792f64d183bca
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-12-18 01:03:08 -0800

    - Restored to localstorage on pocketbase.js until further notice

--- libraries/pocketbase.js
+++ libraries/pocketbase.js
@@ -1,33 +1,26 @@
 import PocketBase from 'pocketbase'
-import {useCookies} from 'react-cookie'
 
 const pocketbase = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL)
 
-const useAuthCookies = () => {
-  const [cookies, setCookie, removeCookie] = useCookies(['auth.token', 'auth.model', 'auth.expiry'])
+// Set session expiration to 168 hours (1 week).
+pocketbase.authStore.save = (token, model) => {
+    localStorage.setItem(pocketbase.authStore.storageKey, JSON.stringify({
+        token: token,
+        model: model,
+        expiry: Date.now() + 168 * 60 * 60 * 1000
+    }))
+}
 
-  // Set session expiration to 168 hours (1 week).
-  pocketbase.authStore.save = (token, model) => {
-    const expiry = Date.now() + 168 * 60 * 60 * 1000
-    setCookie('auth.token', token, { path: '/', expires: new Date(expiry) })
-    setCookie('auth.model', JSON.stringify(model), { path: '/', expires: new Date(expiry) })
-    setCookie('auth.expiry', expiry, { path: '/', expires: new Date(expiry) })
-  }
+pocketbase.authStore.load = () => {
+    const raw = localStorage.getItem(pocketbase.authStore.storageKey)
+    if (!raw) return
 
-  pocketbase.authStore.load = () => {
-    const expiry = cookies['auth.expiry']
-    if (!expiry || Date.now() >= expiry) {
-      pocketbase.authStore.clear()
-      removeCookie('auth.token')
-      removeCookie('auth.model')
-      removeCookie('auth.expiry')
-    } else {
-      pocketbase.authStore.token = cookies['auth.token']
-      pocketbase.authStore.model = JSON.parse(cookies['auth.model'])
+    const data = JSON.parse(raw)
+    if (Date.now() >= data.expiry) pocketbase.authStore.clear()
+    else {
+        pocketbase.authStore.token = data.token
+        pocketbase.authStore.model = data.model
     }
-  }
-
-  return { pocketbase }
 }
 
 export default pocketbase
