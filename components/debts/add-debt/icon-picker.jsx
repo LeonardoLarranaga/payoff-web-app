b2b5b6f7b6d9d524338e0761f291983386b61a8d
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-12-19 22:29:59 -0800

    - When opening the icon picker, the search bar focus automatically

--- components/debts/add-debt/icon-picker.jsx
+++ components/debts/add-debt/icon-picker.jsx
@@ -1,11 +1,13 @@
 import {Button, Input, Popover, PopoverContent, PopoverTrigger} from "@nextui-org/react"
 import {Icon} from "@iconify/react"
-import {useEffect, useState} from "react"
+import {useEffect, useRef, useState} from "react"
 
 export default function IconPicker({icon, setIcon}) {
 
+    const inputRef = useRef(null)
     const [icons, setIcons] = useState([])
     const [query, setQuery] = useState("")
+    const [isOpened, setIsOpened] = useState(false)
     const [abortController, setAbortController] = useState(new AbortController())
 
     const fetchIcons = async () => {
@@ -33,8 +35,12 @@ export default function IconPicker({icon, setIcon}) {
         return () => clearTimeout(timeout)
     }, [query])
 
+    useEffect(() => {
+        if (isOpened) inputRef.current.focus()
+    }, [isOpened])
+
     return (
-        <Popover>
+        <Popover onOpenChange={setIsOpened}>
             <PopoverTrigger className="grid-cols-1 h-14">
                 <Button
                     variant="bordered"
@@ -54,8 +60,9 @@ export default function IconPicker({icon, setIcon}) {
                     <Input
                         label="Search"
                         variant="underlined"
+                        ref={inputRef}
                         onValueChange={setQuery}
-                        className="mb-4"
+                        className="mb-2"
                         startContent={<svg xmlns="http://www.w3.org/2000/svg" width={18} height={18} viewBox="0 0 24 24"><g fill="none" fillRule="evenodd"> <path d="m12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035q-.016-.005-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093q.019.005.029-.008l.004-.014l-.034-.614q-.005-.018-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z"></path> <path fill="currentColor" d="M10.5 2a8.5 8.5 0 1 0 5.262 15.176l3.652 3.652a1 1 0 0 0 1.414-1.414l-3.652-3.652A8.5 8.5 0 0 0 10.5 2M4 10.5a6.5 6.5 0 1 1 13 0a6.5 6.5 0 0 1-13 0"></path></g></svg>}
                     />
 
@@ -72,7 +79,6 @@ export default function IconPicker({icon, setIcon}) {
                             </Button>
                         ))}
                     </div>
-
                 </div>
             </PopoverContent>
         </Popover>
