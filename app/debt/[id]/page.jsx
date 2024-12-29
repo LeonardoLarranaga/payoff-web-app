4b402ce887d7f32a7f53118618b9b02a2b7dd95e
Leonardo Larrañaga <leonardolarranaga@icloud.com>
2024-12-28 21:33:51 -0800

    - Fetch debt

--- app/debt/[id]/page.jsx
+++ app/debt/[id]/page.jsx
@@ -1,9 +1,35 @@
-export default function GetDebt({params: {id}}) {
+'use client'
+
+import {use, useEffect, useState} from "react"
+import pocketbase from "@/libraries/pocketbase"
+
+export default function GetDebt({params}) {
+
+    const { id } = use(params)
+    const [debt, setDebt] = useState({})
+
+    useEffect(() => {
+        const fetchDebt = async () => {
+            try {
+                const record = await pocketbase.collection("debts").getOne(id, {
+                    expand: 'transactions'
+                })
+                setDebt(record)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        fetchDebt().catch()
+    }, [id])
 
     return (
-        <div>
-            <h1>Get Debt</h1>
-            <h2>{id}</h2>
+        <div className="max-h-svh min-h-svh w-full">
+            <div className="sm:pl-4 sm:pt-2">
+                <h1>Get Debt</h1>
+                <h1>{id}</h1>
+                <h1>{JSON.stringify(debt)}</h1>
+            </div>
         </div>
     )
 }
