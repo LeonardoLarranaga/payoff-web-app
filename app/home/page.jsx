df44ed4206fb916d5b9f51159efbbfdcdcc7ff26
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-01-11 19:39:05 -0800

    - home under construction

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,66 +1,67 @@
 'use client'
 
-import pocketbase from "@/libraries/pocketbase"
-import {useEffect, useState} from "react"
-import MyResponsiveLine from "@/components/MyResponsiveLine"
-import {motion} from "framer-motion"
+// import pocketbase from "@/libraries/pocketbase"
+// import {useEffect, useState} from "react"
+// import MyResponsiveLine from "@/components/MyResponsiveLine"
+// import {motion} from "framer-motion"
 
 export default function Home() {
-    const [totalDebt, setTotalDebt] = useState(null)
-    const [failed, setFailed] = useState(false)
-    const [debtHistory, setDebtHistory] = useState(null)
-    const [hasFetch, setHasFetch] = useState(false)
+    // const [totalDebt, setTotalDebt] = useState(null)
+    // const [failed, setFailed] = useState(false)
+    // const [debtHistory, setDebtHistory] = useState(null)
+    // const [hasFetch, setHasFetch] = useState(false)
 
-    useEffect(() => {
-        if (hasFetch) return
-
-        async function fetchTotalDebt() {
-            try {
-                if (hasFetch || !pocketbase.authStore.isValid) return
-                const user = await pocketbase.collection('users').getOne(pocketbase.authStore.record.id)
-                setTotalDebt(user.totalDebt)
-                setDebtHistory(user.debtHistory)
-                setHasFetch(true)
-            } catch (error) {
-                if (error.toString().includes("autocancel")) return
-                console.error(error)
-                setFailed(true)
-            }
-        }
-
-        fetchTotalDebt().catch(console.error)
-    }, [hasFetch])
+    // useEffect(() => {
+    //     if (hasFetch) return
+    //
+    //     async function fetchTotalDebt() {
+    //         try {
+    //             if (hasFetch || !pocketbase.authStore.isValid) return
+    //             const user = await pocketbase.collection('users').getOne(pocketbase.authStore.record.id)
+    //             setTotalDebt(user.totalDebt)
+    //             setDebtHistory(user.debtHistory)
+    //             setHasFetch(true)
+    //         } catch (error) {
+    //             if (error.toString().includes("autocancel")) return
+    //             console.error(error)
+    //             setFailed(true)
+    //         }
+    //     }
+    //
+    //     fetchTotalDebt().catch(console.error)
+    // }, [hasFetch])
 
     return (
         <div className="max-h-svh min-h-svh w-full">
-            <div
-                className="flex flex-col gap-4 items-center justify-center sm:grid sm:grid-cols-7 sm:gap-2 w-full sm:w-auto">
-                {failed && <span className="text-danger">Error retreiving debt</span>}
-                {totalDebt && (
-                    <motion.div
-                        className="relative w-full flex flex-row items-center sm:items-end justify-center sm:col-span-3"
-                        initial={ {opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.8 }}
-                    >
-                        <div className="flex flex-col items-center sm:mr-8">
-                            <h1 className="font-semibold text-gray-300 opacity-35 text-4xl">your balance</h1>
-                            <h1 className="text-7xl font-extrabold text-center">${totalDebt}</h1>
-                        </div>
-                    </motion.div>
-                )}
+            {/*<div*/}
+            {/*    className="flex flex-col gap-4 items-center justify-center sm:grid sm:grid-cols-7 sm:gap-2 w-full sm:w-auto">*/}
+                {/*{failed && <span className="text-danger">Error retreiving debt</span>}*/}
+                {/*{totalDebt && (*/}
+                {/*    <motion.div*/}
+                {/*        className="relative w-full flex flex-row items-center sm:items-end justify-center sm:col-span-3"*/}
+                {/*        initial={ {opacity: 0, y: 20 }}*/}
+                {/*        animate={{ opacity: 1, y: 0 }}*/}
+                {/*        transition={{ duration: 0.8 }}*/}
+                {/*    >*/}
+                {/*        <div className="flex flex-col items-center sm:mr-8">*/}
+                {/*            <h1 className="font-semibold text-gray-300 opacity-35 text-4xl">your balance</h1>*/}
+                {/*            <h1 className="text-7xl font-extrabold text-center">${totalDebt}</h1>*/}
+                {/*        </div>*/}
+                {/*    </motion.div>*/}
+                {/*)}*/}
 
-                {debtHistory && (
-                    <motion.div
-                        className="sm:col-span-4 w-full sm:-ml-6"
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={{ opacity: 1, y: 0 }}
-                        transition={{ duration: 0.8 }}
-                    >
-                        <MyResponsiveLine data={debtHistory}/>
-                    </motion.div>
-                )}
-            </div>
+                {/*{debtHistory && (*/}
+                {/*    <motion.div*/}
+                {/*        className="sm:col-span-4 w-full sm:-ml-6"*/}
+                {/*        initial={{ opacity: 0, y: 20 }}*/}
+                {/*        animate={{ opacity: 1, y: 0 }}*/}
+                {/*        transition={{ duration: 0.8 }}*/}
+                {/*    >*/}
+                {/*        <MyResponsiveLine data={debtHistory}/>*/}
+                {/*    </motion.div>*/}
+                {/*)}*/}
+            {/*</div>*/}
+            <p className="p-12">Home is under construction - Create a debt in the sidebar</p>
         </div>
     )
 }
